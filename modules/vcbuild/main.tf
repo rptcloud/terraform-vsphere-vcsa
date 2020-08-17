68069d149fdaf14a60cf62127956ff49a5a58bad
fparry <fparry@riverpointtechnology.com>
2020-08-17 11:42:48 -0400

    update code for Linux installer

--- modules/vcbuild/main.tf
+++ modules/vcbuild/main.tf
@@ -1,34 +1,34 @@
 locals {
   vctemplate_out = templatefile("${path.module}/template/vctemplate.json.tpl", {
-    esxihostname = var.vcbuild_esxihostname
-	esxiusername = var.vcbuild_esxiusername
-	esxipassword = var.vcbuild_esxipassword
-	deployment_network = var.vcbuild_deployment_network
-	datastore = var.vcbuild_datastore
-	thin_disk_mode = var.vcbuild_thin_disk_mode
-	deployment_option = var.vcbuild_deployment_option
-	vchostname = var.vcbuild_vchostname
-	ip_family = var.vcbuild_ip_family
-	network_mode = var.vcbuild_network_mode
-	vcrootpassword = var.vcbuild_vcrootpassword
-	vcssopassword = var.vcbuild_vcssopassword
-	ntp_servers = var.vcbuild_ntp_servers
-	ssh_enable = var.vcbuild_ssh_enable
-	sso_domain_name = var.vcbuild_sso_domain_name
-	vcfqdn = var.vcbuild_vcfqdn
-	vcipaddress = var.vcbuild_vcip
-	dnsserver = var.vcbuild_dnsserver
-	ipprefix = var.vcbuild_ipprefix
-	gateway = var.vcbuild_gateway
-	ceip_enabled = var.vcbuild_ceip_enabled
+    esxihostname       = var.vcbuild_esxihostname
+    esxiusername       = var.vcbuild_esxiusername
+    esxipassword       = var.vcbuild_esxipassword
+    deployment_network = var.vcbuild_deployment_network
+    datastore          = var.vcbuild_datastore
+    thin_disk_mode     = var.vcbuild_thin_disk_mode
+    deployment_option  = var.vcbuild_deployment_option
+    vchostname         = var.vcbuild_vchostname
+    ip_family          = var.vcbuild_ip_family
+    network_mode       = var.vcbuild_network_mode
+    vcrootpassword     = var.vcbuild_vcrootpassword
+    vcssopassword      = var.vcbuild_vcssopassword
+    ntp_servers        = var.vcbuild_ntp_servers
+    ssh_enable         = var.vcbuild_ssh_enable
+    sso_domain_name    = var.vcbuild_sso_domain_name
+    vcfqdn             = var.vcbuild_vcfqdn
+    vcipaddress        = var.vcbuild_vcip
+    dnsserver          = var.vcbuild_dnsserver
+    ipprefix           = var.vcbuild_ipprefix
+    gateway            = var.vcbuild_gateway
+    ceip_enabled       = var.vcbuild_ceip_enabled
   })
 }
 resource "local_file" "vcbuild_output" {
-  filename = "C:\\vcenter\\vctemplate.json"
+  filename = "/home/runner/vctemplate.json"
   content  = local.vctemplate_out
 }
 resource "null_resource" "vc" {
-   provisioner "local-exec" {
-     command = "C:\\vcenter\\m-vmvcs7\\vcsa-cli-installer\\win32\\vcsa-deploy.exe install --accept-eula --acknowledge-ceip --terse --no-ssl-certificate-verification c:\\vcenter\\vctemplate.json"
-   }
- }
+  provisioner "local-exec" {
+    command = "/home/runner/vcenter/vcsa-cli-installer/lin64/vcsa-deploy install --accept-eula --acknowledge-ceip --no-ssl-certificate-verification --terse /home/runner/vctemplate.json"
+  }
+}
--- modules/vcbuild/variables.tf
+++ modules/vcbuild/variables.tf
@@ -1,104 +1,104 @@
 variable "vcbuild_esxihostname" {
   description = "ESXi hostname to deploy VCSA"
-  default = "192.168.169.34"
+  default     = "192.168.169.34"
 }
 
 variable "vcbuild_esxiusername" {
   description = "ESXi username to deploy VCSA"
-  default = "root"
+  default     = "root"
 }
 
 variable "vcbuild_sso_domain_name" {
   description = "SSO domain name"
-  default = "vsphere.local"
+  default     = "vsphere.local"
 }
 
 variable "vcbuild_deployment_network" {
   description = "Network to deploy VCSA"
-  default = "VM Network"
+  default     = "VM Network"
 }
 
 variable "vcbuild_thin_disk_mode" {
   description = "Use thin disks for VCSA?  Valid options are 'true' or 'false' "
-  default = true
+  default     = false
 }
 
 variable "vcbuild_deployment_option" {
   description = "VCSA sizing.  Valid options are 'tiny', 'small', 'medium', 'large', 'x-large' "
-  default = "tiny"
+  default     = "small"
 }
 
 variable "vcbuild_ip_family" {
   description = "ipv4 or ipv6"
-  default = "ipv4"
+  default     = "ipv4"
 }
 
 variable "vcbuild_network_mode" {
   description = "static or dhcp"
-  default = "static"
+  default     = "static"
 }
 
 variable "vcbuild_datastore" {
   description = "Datastore to deploy VCSA"
-  default = "raid10_gen7"
+  default     = "raid10_gen7"
 }
 
 variable "vcbuild_vchostname" {
   description = "vCenter host name"
-  default = "tfvcenter01"
+  default     = "tfvcenter01"
 }
 
 variable "vcbuild_vcrootpassword" {
   description = "Root password for vcenter server"
-  default = "$$hRPTpass123!"
+  default     = "$$hRPTpass123!"
 }
 
 variable "vcbuild_vcssopassword" {
   description = "SSO password for vcenter server"
-  default = "RPTpass123!"
+  default     = "RPTpass123!"
 }
 
 variable "vcbuild_ntp_servers" {
   description = "NTP servers"
-  default = "time.nist.gov"
+  default     = "time.nist.gov"
 }
 
 variable "vcbuild_ssh_enable" {
   description = "Enable SSH?  'true' or 'false' "
-  default = true
+  default     = true
 }
 
 variable "vcbuild_esxipassword" {
   description = "esxi password"
-  default = "RPTpass123!"
+  default     = "RPTpass123!"
 }
 
 variable "vcbuild_vcip" {
   description = "ip address"
-  default = "192.168.169.55"
+  default     = "192.168.169.55"
 }
 
 variable "vcbuild_dnsserver" {
   description = "dns server"
-  default = "192.168.169.41"
+  default     = "8.8.8.8"
 }
 
 variable "vcbuild_ipprefix" {
   description = "Network prefix length (bits for subnet mask). Use only when the mode is 'static'."
-  default = "24"
+  default     = "24"
 }
 
 variable "vcbuild_gateway" {
   description = "gateway"
-  default = "192.168.169.3"
+  default     = "192.168.169.3"
 }
 
 variable "vcbuild_vcfqdn" {
   description = "FQDN or IP address for the appliance."
-  default = "192.168.169.55"
+  default     = "192.168.169.55"
 }
 
 variable "vcbuild_ceip_enabled" {
   description = "Join VMware Customer Experience Program? 'true' or 'false' "
-  default = false
+  default     = false
 }
