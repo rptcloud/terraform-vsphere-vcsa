0ba3cacb7d7d1af260d914a2f8edaa293216d826
fparry <fparry@riverpointtechnology.com>
2020-08-20 16:36:26 -0400

    add outputs

--- output.tf
+++ output.tf
@@ -1,21 +1,22 @@
 output "vcenter_hostname" {
   value       = module.vcbuild.vcenter_hostname
-  description = "The vCenter Hostname is:"
+  description = "The vCenter Hostname"
 }
 output "vcenter_ip_address" {
   value       = module.vcbuild.vcenter_ip_address
-  description = "The vCenter Management IP Address is:"
+  description = "The vCenter Management IP Address"
 }
 output "vcenter_username" {
   value       = module.vcbuild.vcenter_username
-  description = "The vCenter SSO username is:"
+  description = "The vCenter SSO username"
 }
 output "vcenter_password" {
   value       = module.vcbuild.vcenter_password
-  description = "The vCenter SSO password is:"
+  sensitive = true
+  description = "The vCenter SSO password"
 }
 output "vcenter_rootpassword" {
   value       = module.vcbuild.vcenter_rootpassword
   sensitive   = true
-  description = "The vCenter root password is:"
+  description = "The vCenter root password"
 }
