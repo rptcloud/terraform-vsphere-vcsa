bde272a3442150b22bc318817cfe3508c2ef98d5
Frank Parry <fparry@riverpointtechnology.com>
2020-09-04 11:20:33 -0400

    add MIT license information

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+################################################################################
+################################################################################
+################################################################################
+###                                                                          ###
+### Name: terraform-vsphere-vcsa                                             ###
+### Description: [Terraform] Module to create and deploy VMware VCSA         ###
+### Last Modified: fparry(2020-09-04T11:12:56-04:00)                         ###
+### License: MIT (See LICENSE.txt in the root of this repository for more    ###
+###   information.)                                                          ###
+###                                                                          ###
+################################################################################
+################################################################################
+################################################################################
+
 locals {
   vctemplate_out = templatefile("${path.module}/template/vctemplate.json.tpl", {
     esxihostname       = var.vcbuild_esxihostname
